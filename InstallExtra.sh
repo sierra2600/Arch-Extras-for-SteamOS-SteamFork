1118e3f0517dd1d0df68ec06085191b059d6279a
sierra2600 <147783105+sierra2600@users.noreply.github.com>
2026-02-01 16:30:58 -0600

    Update InstallExtra.sh
    added commented out note

--- InstallExtra.sh
+++ InstallExtra.sh
@@ -41,3 +41,4 @@ else
 	passwd
 	exit
 fi
+# commands to add: passwd && sudo steamos-readonly disable && sudo pacman-key --init && sudo pacman-key --populate archlinux && sudo pacman-key --populate holo && sudo pacman -S <linker>
