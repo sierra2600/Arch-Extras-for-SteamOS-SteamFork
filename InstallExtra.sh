1b8e073f4274a5d33167ff666305a616a992cf56
sierra2600 <147783105+sierra2600@users.noreply.github.com>
2026-02-02 21:55:17 -0600

    Update InstallExtra.sh

--- InstallExtra.sh
+++ InstallExtra.sh
@@ -1,13 +1,18 @@
-exit  #This script is not ready to be used
 #!/bin/bash
+export notReady="We're sorry but this script is not ready for use. Have a nice day!"
+echo -e "${notReady}"
+zenity --error --text="${notReady}"
+exit
 
+export preHandRep="We're sorry but it appears that "
+export handRepeat=", from personal experience, I will recommend doing further reading into what you want to be doing in order to prevent inoperablity to and data loss on your system. Have a nice day!"
 # Checking if this is being ran in Desktop Mode or ssh / virtual tty session
 xdpyinfo &> /dev/null
 if [ $? -eq 0 ]
 then
 	# Running in Desktop Mode, will continue past this 'if then'
 else
-  echo -e "We're sorry but it appears that this is not being ran in Desktop mode, from personal experience, I will recommend doing further reading into what you want to be doing in order to prevent inoperablity to and data loss on your system. Have a nice day!"
+	echo -e "${preHandRep}this is not being ran in Desktop mode${handRepeat}"
 	# insert script self removal call here
 	exit
 fi
@@ -19,8 +24,9 @@ if [ $? -ne 0 ]
 then
 	# SteamOS?SteamFork 3.6.x or 3.7.x found, will continue past this 'if then'
 else
-	echo "We're sorry but it appears that your system is running an outdated version of SteamOS/SteamFork ( $steamos_version ), from personal experience, I will recommend doing further reading into what you want to be doing in order to prevent inoperablity to and data loss on your system. Have a nice day! (This will only work on SteamOS/SteamFork. Are you trying to run this on Arch? Get out here, go on, get, you're silly)"
-	zenity --error --text="We're sorry but it appears that your system is running an outdated version of SteamOS/SteamFork ( $steamos_version ), from personal experience, I will recommend doing further reading into what you want to be doing in order to prevent inoperablity to and data loss on your system. Have a nice day! (This will only work on SteamOS/SteamFork. Are you trying to run this on Arch? Get out here, go on, get, you're silly)"
+	export oldMess="${preHandRep}your system is running an outdated version of SteamOS/SteamFork ( $steamos_version )${handRepeat}"
+	echo -e "${oldMess}"
+	zenity --error --text="${oldMess}"
 	# insert script self removal call here
 	exit
 fi
@@ -29,22 +35,35 @@ fi
 if [ "$(passwd --status $(whoami) | tr -s " " | cut -d " " -f 2)" == "P" ]
 then
 	read -s -p "Please enter current sudo password: " current_password ; echo
-	echo Checking if the sudo password is correct.
-	echo -e "$current_password\n" | sudo -S -k ls &> /dev/null
+	echo -e "Checking if the sudo password is correct"
+	# echo -e "$current_password\n" | sudo -S -k ls &> /dev/null   # known working
+	zenity --password | sudo -S -k ls &> /dev/null
 
 	if [ $? -eq 0 ]
 	then
-		echo Sudo password is good!
+		echo -e "Sudo password is good!"
 	else
-		zenity --error --text="We're sorry but it appears that the password you've entered is incorrect, from personal experience, I will recommend doing further reading into what you want to be doing in order to prevent inoperablity to and data loss on your system. Have a nice day!"
+		export WrongPass="${preHandRep}the password you've entered is incorrect${handRepeat}"
+		echo -e "${WrongPass}"
+		zenity --error --text="${WrongPass}"
 		# insert script self removal call here
 		exit
 	fi
 else
-	echo Sudo password is blank! Setup a sudo password first and then re-run script!
-	zenity --error --text="We're sorry but it appears that the Administrator (sudo) password has not been set"
-	
-	passwd
+	export AdNotSet="${preHandRep}the Administrator (sudo) password has not been set${handRepeat}"
+	echo -e "${AdNotSet}"
+	zenity --error --text="${AdNotSet}"
+	# insert script self removal call here
+	# "passwd"
 	exit
 fi
+
+# Insert code here
+
+if [ ! $? = 0 ]; then
+    echo "USER: Operation cancelled by the user."
+	# insert script self removal call here
+    exit 0
+fi
+
 # commands to add: sudo steamos-readonly disable && sudo pacman-key --init && sudo pacman-key --populate archlinux && sudo pacman-key --populate holo && sudo pacman -S <linker>
